f6e92825adbb16b2df0d12a059c09fa883e260d2
Quentin GOMES DOS REIS <quentin.gomes.dev@gmail.com>
2022-11-01 22:05:10 +0100

    Add PostIn and PostOut classes that will be usefull for parameters in APIMethods

---
+++ src/main/java/tinygram/entities/PostIn.java
@@ -0,0 +1,6 @@
+package tinygram.entities;
+
+public class PostIn {
+    public String pictureURL;
+    public String body;
+}
---
+++ src/main/java/tinygram/entities/PostOut.java
@@ -0,0 +1,9 @@
+package tinygram.entities;
+
+import java.util.Date;
+
+public class PostOut {
+    public String body;
+    public String pictureURL;
+    public Date creationDate;
+}
